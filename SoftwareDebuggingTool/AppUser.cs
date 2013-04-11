119b829a4c061fb0b7ec4b59c74d5425c4a3bbd1
Francois Hill <francoishill11@gmail.com>
2013-04-11 13:39:14 +0200

    Initial commit.

---
+++ SoftwareDebuggingTool/AppUser.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using SharedClasses;
+
+namespace SoftwareDebuggingTool
+{
+	public class AppUser : INotifyPropertyChanged
+	{
+		private const string cPhotoFileName = "Photo.jpg";
+		private const int cInterfaceXmlModifiedMinutesAgo = 30;
+		private const int cAppDllModifiedMinutesAgo = 120;
+		private const string cTodaysFoldernameDateFormat = "yyyy-MM-dd";
+		private const string cRootBinariesDir = @"C:\devKiln\build_albion\tundra-output\win32-baked-rtest-default";
+
+		public static readonly string cApplicationName = SettingsSimple.SoftwareDebuggingTool.Instance.ApplicationName;
+		public static readonly int cApplicationVersion = SettingsSimple.SoftwareDebuggingTool.Instance.ApplicationVersion;
+		public static readonly string cSharedFoldersRoot = SettingsSimple.SoftwareDebuggingTool.Instance.SharedFoldersRoot;//@"R:\Francois\Debug";
+		public static readonly string cLocalFoldersRoot = SettingsSimple.SoftwareDebuggingTool.Instance.LocalFoldersRoot;//@"C:\Programming\Wadiso6\Debug data";
+
+		public string Name { get; private set; }
+		public string PhotoFile { get; private set; }
+		public bool HasLocalFolder { get { return Directory.Exists(GetLocalFolderPath()); } }
+		public bool HasSharedFolder { get { return Directory.Exists(GetSharedFolderPath()); } }
+
+		public AppUser(string Name)
+		{
+			this.Name = Name;
+			this.CheckIfHasPhotoFile();
+		}
+
+		private void CheckIfHasPhotoFile()
+		{
+			string tmpPhotoFile = Path.Combine(cSharedFoldersRoot, this.Name, cPhotoFileName);
+			if (File.Exists(tmpPhotoFile))
+				this.PhotoFile = tmpPhotoFile;
+		}
+
+		private string GetLocalFolderPath() { return Path.Combine(cLocalFoldersRoot, this.Name); }
+		private string GetSharedFolderPath() { return Path.Combine(cSharedFoldersRoot, this.Name); }
+
+		private static void _ensureDirectoryOfFileExists(string filepath)
+		{
+			string dir = Path.GetDirectoryName(filepath);
+			if (!Directory.Exists(dir))
+				Directory.CreateDirectory(dir);
+		}
+
+		private T DoActionCatchException<T>(Func<T> action, T returnValueIfError, string actionDescriptorUserInErrorMessage)
+		{
+			try
+			{
+				return action();
+			}
+			catch (Exception exc)
+			{
+				UserMessages.ShowErrorMessage(string.Format("{0} for '{1}': {2}{3}{4}{5}",
+					actionDescriptorUserInErrorMessage, this.Name, exc.Message, Environment.NewLine, exc.StackTrace));
+				return returnValueIfError;
+			}
+		}
+		private void DoActionCatchException(Action action, string actionDescriptorUserInErrorMessage)
+		{
+			DoActionCatchException<object>(delegate { action(); return null; }, null, actionDescriptorUserInErrorMessage);
+		}
+
+		private string GetTodaysDirectoryInLocalFolder()
+		{
+			return DoActionCatchException<string>(
+				delegate
+				{
+					string dir = Path.Combine(this.GetLocalFolderPath(), DateTime.Now.ToString(cTodaysFoldernameDateFormat));
+					if (!Directory.Exists(dir))
+						Directory.CreateDirectory(dir);
+					return dir;
+				},
+				null,
+				"Error getting today's directory in Local folder");
+		}
+
+		private string GetTodaysDirectoryInSharedFolder()
+		{
+			return DoActionCatchException<string>(
+				delegate
+				{
+					string dir = Path.Combine(this.GetSharedFolderPath(), DateTime.Now.ToString(cTodaysFoldernameDateFormat));
+					if (!Directory.Exists(dir))
+						Directory.CreateDirectory(dir);
+					return dir;
+				},
+				null,
+				"Error getting today's directory in Shared folder");
+		}
+
+		private void ExploreTo(string directory)
+		{
+			ThreadingInterop.PerformOneArgFunctionSeperateThread<string>(
+				dir => Process.Start(dir),
+				directory,
+				false);
+			/*ThreadingInterop.PerformOneArgFunctionSeperateThread<string>(
+				dir => Process.Start("explorer", "/select,\"" + dir + "\""),
+				directory,
+				false);*/
+		}
+
+		public void ExploreToLocalFolder() { ExploreTo(GetLocalFolderPath()); }
+
+		public void CreateLocalFolder()
+		{
+			DoActionCatchException(delegate
+			{
+				Directory.CreateDirectory(this.GetLocalFolderPath());
+				OnPropertyChanged(u => u.HasLocalFolder);
+			},
+			"Create local folder");
+		}
+
+		public void ExploreToSharedFolder() { ExploreTo(GetSharedFolderPath()); }
+
+		public void CreateSharedFolder()
+		{
+			DoActionCatchException(delegate
+			{
+				Directory.CreateDirectory(this.GetSharedFolderPath());
+				OnPropertyChanged(u => u.HasSharedFolder);
+			},
+			"Create shared folder");
+		}
+
+		private void _copyBinaryFileToTodaysSharedFolder(string filenameOnly, string fileIdentifierForUsermessages, Func<string, bool> checkOnSourceFile)
+		{
+			DoActionCatchException(
+				   delegate
+				   {
+					   string sourceDllPath = Path.Combine(cRootBinariesDir, filenameOnly);
+					   if (!File.Exists(sourceDllPath))
+						   UserMessages.ShowWarningMessage("File does not exist: " + sourceDllPath);
+					   if (!checkOnSourceFile(sourceDllPath))
+						   return;
+
+					   string todaysDirectoryInShared = GetTodaysDirectoryInSharedFolder();
+					   if (todaysDirectoryInShared == null) return;
+					   string destinationFile = Path.Combine(todaysDirectoryInShared, "ProgramFiles", filenameOnly);
+					   _ensureDirectoryOfFileExists(destinationFile);
+					   File.Copy(sourceDllPath, destinationFile, true);
+					   if (UserMessages.Confirm("Successfully copied " + fileIdentifierForUsermessages + " to shared folder of " + this.Name + ", do you want to place the directory path in your clipboard?", "Set clipboard to directory path"))
+						   Clipboard.SetText(Path.GetDirectoryName(destinationFile));
+				   },
+			   "Could not copy DLL");
+		}
+
+		public void Copy32bitRTestDllToShared()
+		{
+			var filenameOnly = string.Format("{0}{1}.dll", cApplicationName, cApplicationVersion);
+			_copyBinaryFileToTodaysSharedFolder(
+				filenameOnly,
+				"32bit RTest DLL",
+				(sourceFileToCheck) =>
+				{
+					var lastModifiedDate = File.GetLastWriteTime(sourceFileToCheck);
+					return DateTime.Now.Subtract(lastModifiedDate).TotalMinutes < cAppDllModifiedMinutesAgo
+						|| UserMessages.Confirm(string.Format("The file '{0}' has been modified more than {1} minutes ago, do you want to use this file?", sourceFileToCheck, cAppDllModifiedMinutesAgo), "File modified a while ago");
+				});
+		}
+
+		public void CopyXmlInterfaceToShared()
+		{
+			var filenameOnly = string.Format("{0}{1}Interface.xml", cApplicationName, cApplicationVersion);
+			_copyBinaryFileToTodaysSharedFolder(
+				filenameOnly,
+				filenameOnly,
+				(sourceFileToCheck) =>
+				{
+					var lastModifiedDate = File.GetLastWriteTime(sourceFileToCheck);
+					return DateTime.Now.Subtract(lastModifiedDate).TotalMinutes < cInterfaceXmlModifiedMinutesAgo
+						|| UserMessages.Confirm(string.Format("The file '{0}' has been modified more than {1} minutes ago, do you want to use this file?", sourceFileToCheck, cInterfaceXmlModifiedMinutesAgo), "File modified a while ago");
+				});
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged = delegate { };
+		private void OnPropertyChanged(params Expression<Func<AppUser, object>>[] propertiesOrFieldsAsExpressions)
+		{
+			ReflectionInterop.DoForeachPropertOrField<AppUser>(
+				this,
+				propertiesOrFieldsAsExpressions,
+				(instanceObj, memberInfo, memberValue) =>
+				{
+					PropertyChanged(instanceObj, new PropertyChangedEventArgs(memberInfo.Name));
+				});
+		}
+	}
+}
---
+++ SoftwareDebuggingTool/MainWindow.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using SharedClasses;
+
+namespace SoftwareDebuggingTool
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private ObservableCollection<AppUser> usersList = new ObservableCollection<AppUser>();
+		private System.Windows.WindowState? stateBeforeMinimized = null;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			usersList.CollectionChanged += usersList_CollectionChanged;
+		}
+
+		private bool CanUseFolderNameAsPersonFolder(string foldername)
+		{
+			if (foldername.Equals("_old", StringComparison.InvariantCultureIgnoreCase))
+				return false;
+			else if (foldername.Equals("_own", StringComparison.InvariantCultureIgnoreCase))
+				return false;
+			else
+				return true;
+		}
+
+		private void Window_Loaded_1(object sender, RoutedEventArgs e)
+		{
+			if (this.WindowState != System.Windows.WindowState.Minimized)
+				stateBeforeMinimized = this.WindowState;
+
+			var folderNames = 
+				Directory.GetDirectories(AppUser.cLocalFoldersRoot).Select(dir => Path.GetFileName(dir))
+				.Concat(Directory.GetDirectories(AppUser.cSharedFoldersRoot).Select(dir => Path.GetFileName(dir)))
+				.Where(fn => CanUseFolderNameAsPersonFolder(fn))
+				.OrderBy(s => s)
+				.Distinct();
+			foreach (var foldername in folderNames)
+				usersList.Add(new AppUser(foldername));
+
+			listboxUsers.ItemsSource = usersList;
+		}
+
+		private void HideWindow()
+		{
+			this.Hide();
+		}
+		private void ShowWindow()
+		{
+			this.Show();
+			if (stateBeforeMinimized.HasValue)
+				this.WindowState = stateBeforeMinimized.Value;
+			this.Activate();
+		}
+
+		private void usersList_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+		{
+			trayiconMenuItemUsers.MenuItems.Clear();
+
+			foreach (var user in usersList)
+			{
+				var userMenu = trayiconMenuItemUsers.MenuItems.Add(user.Name);
+				//userMenu.MenuItems.Add("Explore to local
+			}
+		}
+
+		private void Window_StateChanged_1(object sender, EventArgs e)
+		{
+			if (this.WindowState == System.Windows.WindowState.Minimized)
+				HideWindow();
+			else
+				stateBeforeMinimized = this.WindowState;
+		}
+
+		private void Border_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+		{
+			MessageBox.Show("Border clicked");
+		}
+
+		private void menuitemAbout_Click(object sender, RoutedEventArgs e)
+		{
+			AboutWindow2.ShowAboutWindow(new System.Collections.ObjectModel.ObservableCollection<DisplayItem>()
+			{
+				new DisplayItem("Author", "Francois Hill"),
+				new DisplayItem("Icon(s) obtained from", "http://www.iconarchive.com/show/crystal-clear-icons-by-everaldo.html", "http://www.iconarchive.com/show/crystal-clear-icons-by-everaldo.html")
+			});
+		}
+
+		private void menuitemExit_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void menuitemExploreToLocalFolder_Click(object sender, RoutedEventArgs e)
+		{
+			WPFHelper.DoActionIfObtainedItemFromObjectSender<AppUser>(sender,
+				user => user.ExploreToLocalFolder());
+		}
+
+		private void menuitemCreateLocalFolder_Click(object sender, RoutedEventArgs e)
+		{
+			WPFHelper.DoActionIfObtainedItemFromObjectSender<AppUser>(sender,
+							user => user.CreateLocalFolder());
+		}
+
+		private void menuitemExploreToSharedFolder_Click(object sender, RoutedEventArgs e)
+		{
+			WPFHelper.DoActionIfObtainedItemFromObjectSender<AppUser>(sender,
+				user => user.ExploreToSharedFolder());
+		}
+
+		private void menuitemCreateSharedFolder_Click(object sender, RoutedEventArgs e)
+		{
+			WPFHelper.DoActionIfObtainedItemFromObjectSender<AppUser>(sender,
+				user => user.CreateSharedFolder());
+		}
+
+		private void menuitemCopy32bitRTestDllToShared_Click(object sender, RoutedEventArgs e)
+		{
+			WPFHelper.DoActionIfObtainedItemFromObjectSender<AppUser>(sender,
+				user => user.Copy32bitRTestDllToShared());
+		}
+
+		private void menuitemCopyAppInterfaceXmlToShared_Click(object sender, RoutedEventArgs e)
+		{
+			WPFHelper.DoActionIfObtainedItemFromObjectSender<AppUser>(sender,
+				   user => user.CopyXmlInterfaceToShared());
+		}
+
+		private void OnNotificationArayIconMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			if (this.IsVisible)
+				HideWindow();
+			else
+				ShowWindow();
+		}
+
+		private void OnMenuItemShowClick(object sender, EventArgs e)
+		{
+			ShowWindow();
+		}
+
+		private void OnMenuItemAboutClick(object sender, EventArgs e)
+		{
+			AboutWindow2.ShowAboutWindow(new System.Collections.ObjectModel.ObservableCollection<DisplayItem>()
+			{
+				new DisplayItem("Author", "Francois Hill"),
+				new DisplayItem("Icon(s) obtained from", "http://www.iconarchive.com", "http://www.iconarchive.com/show/tulliana-2-icons-by-umut-pulat/bug-icon.html")
+			});
+		}
+
+		private void OnMenuItemExitClick(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
